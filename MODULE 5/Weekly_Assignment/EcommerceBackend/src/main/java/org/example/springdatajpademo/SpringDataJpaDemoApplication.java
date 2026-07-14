3fdbf998f2be7af3540002b30f7e6d6e250a1498
Renjitha K | NorthernArc <renjitha.k@NorthernArc.com>
2026-07-14 15:52:33 +0530

    Backedn added

---
+++ MODULE 5/Weekly_Assignment/EcommerceBackend/src/main/java/org/example/springdatajpademo/SpringDataJpaDemoApplication.java
@@ -0,0 +1,17 @@
+package org.example.springdatajpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+        "org.example.springdatajpademo.Ecommerce.repository"
+})
+public class SpringDataJpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaDemoApplication.class, args);
+    }
+
+}
